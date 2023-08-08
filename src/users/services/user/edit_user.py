992b2bab2f3a39c9170f221171f219c9b2b1ffd7
Grigory Zabolotny <you@example.com>
2023-08-08 11:57:25 +0300

    fix base64 photo load

--- src/users/services/user/edit_user.py
+++ src/users/services/user/edit_user.py
@@ -25,19 +25,20 @@ class EditUserCommandImpl(EditUserCommand):
         self.repo = repo
     
     def __call__(self, user: User, user_data: EditUserEntry) -> User:
-        user = self.set_avatar(user=user, avatar=user_data.avatar)
+        user = self._set_avatar(user=user, avatar=user_data.avatar)
         if user_data.first_name:
             user.first_name = user_data.first_name
         if user_data.last_name:
             user.last_name = user_data.last_name
         return self.repo.save(user=user)
 
-    def set_avatar(self, user: User, avatar: str) -> User:
+    def _set_avatar(self, user: User, avatar: str) -> User:
         if avatar:
             allowed_chars = string.ascii_letters
             length = random.randint(15, 20)
+            _, file_data = avatar.split(';base64,')
             filename = ''.join(random.choice(allowed_chars) for _ in range(length))
-            user.avatar = File(io.BytesIO(base64.b64decode(avatar)), name=filename)
+            user.avatar = File(io.BytesIO(base64.b64decode(file_data)), name=filename)
         if avatar is None:
             user.avatar = None
         return user
